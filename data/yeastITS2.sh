59f1fc2822fd334940e36c16b7b5172ad6958567
Duong Vu <duong.t.vu@gmail.com>
2025-10-22 17:53:14 +0200

    Add files via upload

--- data/yeastITS2.sh
+++ data/yeastITS2.sh
@@ -11,21 +11,20 @@ cd /home/dvu/data/data2/cephstorage/ProgLang/Python/dnabarcoder/alldata/yeastITS
 ../../dnabarcoder.py variation -i yeastITS2.fasta -c yeastITS2.classification -rank class,order,family,genus,species -ml 50 -sim dnabarcoder/yeastITS2.sim
 ../../dnabarcoder.py remove -i yeastITS2.species.fasta -c yeastITS2.classification -rank species -sim dnabarcoder/yeastITS2.sim
 ../../dnabarcoder.py predict -i dnabarcoder/yeastITS2.species.diff.fasta -c yeastITS2.classification -ml 50 -st 0.9 -et 1 -s 0.001 -rank species -prefix yeastITS2 -sim dnabarcoder/yeastITS2.sim
-../../dnabarcoder.py predict -i dnabarcoder/yeastITS2.species.diff.fasta -c yeastITS2.classification -ml 50 -st 0.7 -et 1 -s 0.001 -rank species -prefix yeastITS2 -higherrank genus,family,order,class,phylum -sim dnabarcoder/yeastITS2.sim -mingroupno 10 -maxproportion 0.9 -mincutoff 0.7
+../../dnabarcoder.py predict -i dnabarcoder/yeastITS2.species.diff.fasta -c yeastITS2.classification -ml 50 -st 0.7 -et 1 -s 0.001 -rank species -prefix yeastITS2 -higherrank genus,family,order,class,phylum -sim dnabarcoder/yeastITS2.sim -mingroupno 5 -maxproportion 0.7 -mincutoff 0.7
 ../../dnabarcoder.py predict -i yeastITS2.genus.fasta -c yeastITS2.classification -ml 50 -st 0.7 -et 1 -s 0.001 -rank genus -prefix yeastITS2
-../../dnabarcoder.py predict -i yeastITS2.genus.fasta -c yeastITS2.classification -ml 50 -st 0.7 -et 1 -s 0.001 -rank genus -higherrank family,order,class,phylum -prefix yeastITS2 -mingroupno 10 -maxproportion 0.9
-../../dnabarcoder.py predict -i yeastITS2.family.fasta -c yeastITS2.classification -ml 50 -st 0.5 -et 1 -s 0.001 -rank family -prefix yeastITS2
-../../dnabarcoder.py predict -i yeastITS2.family.fasta -c yeastITS2.classification -ml 50 -st 0.5 -et 1 -s 0.001 -rank family -higherrank order,class,phylum -prefix yeastITS2 -mingroupno 10 -maxproportion 0.9
+../../dnabarcoder.py predict -i yeastITS2.genus.fasta -c yeastITS2.classification -ml 50 -st 0.7 -et 1 -s 0.001 -rank genus -higherrank family,order,class,phylum -prefix yeastITS2 -mingroupno 5 -maxproportion 0.7 -mincutoff 0.7
+../../dnabarcoder.py predict -i yeastITS2.family.fasta -c yeastITS2.classification -ml 50 -st 0.6 -et 1 -s 0.001 -rank family -prefix yeastITS2
+../../dnabarcoder.py predict -i yeastITS2.family.fasta -c yeastITS2.classification -ml 50 -st 0.6 -et 1 -s 0.001 -rank family -higherrank order,class,phylum -prefix yeastITS2 -mingroupno 5 -maxproportion 0.7
 ../../dnabarcoder.py predict -i yeastITS2.order.fasta -c yeastITS2.classification -ml 50 -st 0.5 -et 1 -s 0.001 -rank order -prefix yeastITS2
-../../dnabarcoder.py predict -i yeastITS2.order.fasta -c yeastITS2.classification -ml 50 -st 0.5 -et 1 -s 0.001 -rank order -higherrank class,phylum -prefix yeastITS2 -mingroupno 10 -maxproportion 0.9
+../../dnabarcoder.py predict -i yeastITS2.order.fasta -c yeastITS2.classification -ml 50 -st 0.5 -et 1 -s 0.001 -rank order -higherrank class,phylum -prefix yeastITS2 -mingroupno 5 -maxproportion 0.7
 ../../dnabarcoder.py predict -i yeastITS2.class.fasta -c yeastITS2.classification -ml 50 -st 0.5 -et 1 -s 0.001 -rank class -prefix yeastITS2
-../../dnabarcoder.py predict -i yeastITS2.class.fasta -c yeastITS2.classification -ml 50 -st 0.5 -et 1 -s 0.001 -rank class -higherrank phylum -prefix yeastITS2 -mingroupno 10 -maxproportion 0.9
-../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank species,genus,family,order,class 
-../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank species -higherrank genus -mingroupno 10 -maxproportion 0.9
-../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank genus -higherrank family -mingroupno 10 -maxproportion 0.9
-../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank family -higherrank order -mingroupno 10 -maxproportion 0.9
-../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank order -higherrank class -mingroupno 10 -maxproportion 0.9
-../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank class -higherrank phylum -mingroupno 10 -maxproportion 0.9
+../../dnabarcoder.py predict -i yeastITS2.class.fasta -c yeastITS2.classification -ml 50 -st 0.5 -et 1 -s 0.001 -rank class -higherrank phylum -prefix yeastITS2 -mingroupno 5 -maxproportion 0.7
+../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank genus,family,order,class 
+../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank species -higherrank genus -mingroupno 10 -maxproportion 0.7
+../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank genus -higherrank family -mingroupno 5 -maxproportion 0.7
+../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank family -higherrank order -mingroupno 5 -maxproportion 0.7
+../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank order -higherrank class -mingroupno 5 -maxproportion 0.7
+../../dnabarcoder.py predict -i yeastITS2.fasta -c yeastITS2.classification -rank class -higherrank phylum -mingroupno 5 -maxproportion 0.7
 ../../dnabarcoder.py visualize -i yeastITS2.fasta  -c yeastITS2.classification -rank class -n 8 -size 0.3
-../../dnabarcoder.py best -i dnabarcoder/yeastITS2.cutoffs.json -c yeastITS2.classification 
-
+../../dnabarcoder.py best -i dnabarcoder/yeastITS2.cutoffs.json -c yeastITS2.classification -mincutoff 0.5
